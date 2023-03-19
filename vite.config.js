03e0e029581ec652eeecf4b89fb40eabce2c2df5
Matt Hays <mehays@protonmail.com>
2023-03-19 12:13:52 -0400

    resolve path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'? '/matthays.dev/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/matthays.dev/' : '/matthays.dev/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
