cb9699485e51c5d03164bd6b4733d841b68ffef3
Matt Hays <mehays@protonmail.com>
2023-03-18 20:46:26 -0400

    collapse views

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,8 +1,19 @@
 <template>
+  <v-app>
+    <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" />
+
+      Base Preset
+    </v-app-bar-title>
+  </v-app-bar>
+
   <v-main>
     <router-view />
   </v-main>
+  </v-app>
 </template>
+
 
 <script setup>
   //
