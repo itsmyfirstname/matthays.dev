12bb0a908ac7e9b897217089dbfc47b0cd04e9e0
Matt Hays <mehays@protonmail.com>
2023-03-21 22:48:48 -0400

    cleanup and add animation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,42 @@
 <template>
-  <v-app id="inspire">
-    <v-main class="bg-grey-lighten-3">
-      <div>
-        <v-container class="bg-surface-variant mb-6">
-        <v-row no-gutters style="height: 150px;" align-content="center">
-        <v-col align-self="center">
-          <v-sheet class="pa-2 ma-2">
-            More to come
-          </v-sheet>
-        </v-col>
-      </v-row>
-      </v-container>
-    </div>
-    </v-main>
-  </v-app>
+  <v-container fluid bg>
+    <v-row no-gutters fluid align-content="end">
+      <v-col align-self="center">
+        <v-sheet color="primary" width="30%">
+          <div class="typewriter">
+            <h1>howdy! i'm matt...</h1>
+          </div>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<style scoped>
+
+.typewriter h1 {
+    overflow: scroll; /* Ensures the content is not revealed until the animation */
+    border-right: .15em solid #7CDEDC; /* The typwriter cursor */
+    white-space: nowrap; /* Keeps the content on a single line */
+    margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+    letter-spacing: .15em; /* Adjust as needed */
+    animation: 
+      typing 3.5s steps(30, end),
+      blink-caret .25s step-end infinite;
+  }
+  
+  /* The typing effect */
+  @keyframes typing {
+    from { width: 0 }
+    to { width: 100% }
+  }
+  
+  /* The typewriter cursor effect */
+  @keyframes blink-caret {
+    from, to { border-color: transparent }
+    50% { border-color: #7CDEDC; }
+  }
+</style>
 
 <script>
 // export default {
