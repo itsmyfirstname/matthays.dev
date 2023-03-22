ada9e3bca29d34650f4e3570beec7dd3ec89762f
Matt Hays <mehays@protonmail.com>
2023-03-21 22:48:27 -0400

    testing new color scheme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,16 +11,29 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: '#474954',
+    surface: '#9E788F',
+    primary: '#7284A8',
+    'primary-darken-1': '#6ABEA7',
+    secondary: '#A9B3CE',
+    'secondary-darken-1': '#9FFFF5',
+    highlight: '#7CDEDC',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  }
+}
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: 'myCustomLightTheme',
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
-    },
-  },
+      myCustomLightTheme,
+    }
+  }
 })
