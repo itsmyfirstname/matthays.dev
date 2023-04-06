8a3ec186365de6f65608341013da5dd79f532730
Matt Hays <mehays@protonmail.com>
2023-04-05 23:57:29 -0400

    tie views to router

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,7 +1,19 @@
 <template>
-  <Blog />
+  <v-app id="inspire">
+    <v-main class="bg-grey-lighten-3">
+      <v-container class="bg-surface-variant mb-6">
+        <v-row no-gutters style="height: 150px;" align-content="center">
+        <v-col align-self="center">
+          <v-sheet class="pa-2 ma-2">
+            More to come
+          </v-sheet>
+        </v-col>
+      </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-  import Blog from '@/components/Blog.vue'
+<script>
+
 </script>
