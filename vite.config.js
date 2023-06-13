68d81f0c6184477c660d1712ca12222e3e498dc3
Matt Hays <mehays@protonmail.com>
2023-06-12 22:52:55 -0400

    change output dir

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './_site'
+  },
   publicPath: process.env.NODE_ENV === 'production'? '/matthays.dev/' : '/matthays.dev/',
   plugins: [
     vue({ 
